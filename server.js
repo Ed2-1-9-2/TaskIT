6363219e397ba307201f046792494882ea9be89a
Robert <poparobert22@stud.ase.ro>
2023-11-01 22:26:02 +0200

    Am fixat css-ul

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
-const express = require('express');
+'use strict'
 
-const path = require('path');
+const express = require('express');
+var logger = require('morgan');
+var path = require('path');
+var app = express();
+const port = 8000;
+
+app.use(logger('dev'));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'css')));
 
 const middlewares = require('./api/middlewares');
 const routes = require('./api/routes');
 
-const app = express();
 app.use(express.json());
 
-const port = 8000;
-
 app.listen(port, () => console.log(`Server running at localhost:${port}`));
-
-// TODO: nu se incarca css-ul, trebuie rezolvat
 
 app.use('/api', routes);
 
 app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/formular', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'formular.html'));
+    res.sendFile(path.join(__dirname,  'formular.html'));
 });
 
 app.get('/galerie', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'galerie.html'));
+    res.sendFile(path.join(__dirname,  'galerie.html'));
 });
 
 // Lasam middleware ultimele ca sa functioneze
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
+
+
