6d849eeaad2eeac09107c87dd88c6f02aaff9b6a
AndreiIonica <40264929+andrei-io@users.noreply.github.com>
2023-11-01 17:38:46 +0200

    fix formatting

--- backend/database.js
+++ backend/database.js
@@ -1,67 +1,64 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 const database = new Sequelize("targ_db", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-  logging: false,
-  define: {
-    charset: "utf8",
-    collate: "utf8_general_ci",
-    timestamps: true,
-  },
+    dialect: "mysql",
+    host: "localhost",
+    logging: false,
+    define: {
+        charset: "utf8",
+        collate: "utf8_general_ci",
+        timestamps: true,
+    },
 });
-
 
 const donatorDb = database.define("donator", {
     Id: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
     },
     firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     lastName: {
         type: DataTypes.STRING,
         allowNull: false,
-      },
+    },
     email: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     phone: {
         type: DataTypes.BIGINT,
         allowNull: false,
     },
-  });
-  
-  const donationDb = database.define("donation",
-    {
-      Id: {
+});
+
+const donationDb = database.define("donation", {
+    Id: {
         type: DataTypes.BIGINT,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
-      },
-      donationType: {
+    },
+    donationType: {
         type: DataTypes.STRING,
         allowNull: false,
-      },
-      donationDescription: {
+    },
+    donationDescription: {
         type: DataTypes.TEXT,
         allowNull: true,
-      },
-      donationDate: {
+    },
+    donationDate: {
         type: DataTypes.DATE,
         allowNull: false,
-      },
-      value: {
-        type: DataTypes.FLOAT, 
+    },
+    value: {
+        type: DataTypes.FLOAT,
         allowNull: true,
-      },
-    }
-  );
-  
-  donatorDb.hasMany(donationDb);
+    },
+});
+
+donatorDb.hasMany(donationDb);
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const db = require("./database");
+const express = require('express');
+const db = require('./database');
 const path = require('path');
 
 const app = express();
@@ -9,14 +9,16 @@ const port = 8000;
 
 app.listen(port, () => console.log(`Server running at localhost:${port}`));
 
-app.get("/", async (req, res) => {
+// TODO: nu se incarca css-ul, trebuie rezolvat
+
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
-app.get("/formular", async (req, res) => {
+app.get('/formular', async (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'formular.html'));
 });
 
-app.get("/galerie", async (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend','galerie.html'));
+app.get('/galerie', async (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'galerie.html'));
 });
